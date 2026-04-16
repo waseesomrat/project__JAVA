5792a9d777ce3bf1a9d9ed14e369bf7b98f7a99a
wasi <waseesomrat8@gmail.com>
2026-04-16 16:39:48 +0600

    variables check

---
+++ java/experimental/variables.java
@@ -0,0 +1,24 @@
+public class variables {
+
+
+public static void main(String[] args) {
+
+
+int age = 20;
+double height = 5.8;
+char grade = 'A';
+String name = "wasi";
+int a = 10;
+int b = 15;
+System.out.println("Name: " + name);
+System.out.println("Age: " + age);
+System.out.println("height: " + height);
+System.out.println("Age: " + grade);
+
+// (a + b) first add then concenate 
+// + a + b add all like string
+System.out.println("Sum: " + (a + b)); 
+System.out.println("Age: " + a + b);
+}
+
+}
